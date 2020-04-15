43a94c3c1c8c79bdefa785e2ba0e50f046e53381
Sahil Fruitwala <sh941551@dal.ca>
2020-04-15 17:41:02 -0300

    Calculator with basic functionality done

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,4 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import { Container, Toast, ToastBody, ToastHeader, Row, Col, Label, InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
+// import { ListGroup, ListGroupItem     } from 'reactstrap';
+
 
 const initialState = {
 
@@ -16,10 +19,13 @@ function Calculator() {
     const inputRef2 = useRef(null) // ref to 2nd input
 
     const [number, setNumber] = useState(initialState) // state to change number object
+    const [show, setShow] = useState(false) // to display toast
 
     useEffect(() => {
         inputRef2.current.focus()
     }, [])  // right now of no use
+
+    const toggle = () => setShow(!show);
 
     const setDigit = (digit) => {  // use to append and insert digit into number object 
         if (number.flag) {
@@ -28,7 +34,7 @@ function Calculator() {
         }
         else {
             // check if flag is false than set numberTwo of number object
-            if( number.operation !== '' ) {
+            if (number.operation !== '') {
                 // if operator is empty then assign as new into numberOne of number object
                 setNumber({ ...number, numberOne: digit.trim(), operation: '' })
             }
@@ -59,7 +65,7 @@ function Calculator() {
                     break
                 case 'DIV':
                     if (parseFloat(number.numberTwo) === 0) {
-                        alert('Can not divide by 0!')
+                        setShow(!show);
                         break
                     }
                     setNumber({ numberOne: parseFloat(number.numberOne) / parseFloat(number.numberTwo), numberTwo: '', flag: false })
@@ -72,32 +78,85 @@ function Calculator() {
     }
 
     return (
-        <div>
-            <input type='text'
+        <Container className="themed-container mt-3" fluid="sm" style={{ width: "24%" }}>
+            <Input type="text"
                 value={number.flag ? number.numberOne : '0'}
-                ref={inputRef1} disabled />
-            <input type='text'
-                value={number.flag ? (number.numberTwo === '' ? '0' : number.numberTwo) : (number.numberOne === '' ? '0' : number.numberOne)}
-                ref={inputRef2} disabled />
-            <div>
-                <button onClick={() => setDigit('1')}>1</button>
-                <button onClick={() => setDigit('2')}>2</button>
-                <button onClick={() => setDigit('3')}>3</button>
-                <button onClick={() => setDigit('4')}>4</button>
-                <button onClick={() => setDigit('5')}>5</button>
-                <button onClick={() => setDigit('6')}>6</button>
-                <button onClick={() => setDigit('7')}>7</button>
-                <button onClick={() => setDigit('8')}>8</button>
-                <button onClick={() => setDigit('9')}>9</button>
-                <button onClick={() => setDigit('0')}>0</button>
-                <button onClick={() => operation('ADD')}>+</button>
-                <button onClick={() => operation('SUB')}>-</button>
-                <button onClick={() => operation('MUL')}>x</button>
-                <button onClick={() => operation('DIV')}>/</button>
-                <button onClick={() => operation('ANS')}>=</button>
-            </div>
+                ref={inputRef1}
+                style={{ textAlign: "right" }}
+                disabled />
+            <InputGroup>
+                <InputGroupAddon addonType="prepend"><Label></Label></InputGroupAddon>
+                <Input type="text"
+                    value={number.flag ? (number.numberTwo === '' ? '0' : number.numberTwo) : (number.numberOne === '' ? '0' : number.numberOne)}
+                    ref={inputRef2}
+                    style={{ textAlign: "right" }}
+                    disabled />
+            </InputGroup>
 
-        </div>
+            <Toast isOpen={show}>
+                <ToastHeader icon="danger" toggle={toggle}> Error </ToastHeader>
+                <ToastBody> Division by zero not possible! </ToastBody>
+            </Toast>
+
+            <Row xs="4" className="mt-3">
+                <Col><Button onClick={() => setDigit('1')} color="primary" className={"btn-block"}>1</Button></Col>
+                <Col><Button onClick={() => setDigit('2')} color="primary" className={"btn-block"} >2</Button></Col>
+                <Col><Button onClick={() => setDigit('3')} color="primary" className={"btn-block"} >3</Button></Col>
+                <Col><Button onClick={() => operation('ADD')} color="warning" className={"btn-block"} >+</Button></Col>
+            </Row>
+            <Row xs="4" className="mt-3">
+                <Col><Button onClick={() => setDigit('4')} color="primary" className={"btn-block"} >4</Button></Col>
+                <Col><Button onClick={() => setDigit('5')} color="primary" className={"btn-block"} >5</Button></Col>
+                <Col><Button onClick={() => setDigit('6')} color="primary" className={"btn-block"} >6</Button></Col>
+                <Col><Button onClick={() => operation('SUB')} color="warning" className={"btn-block"} >-</Button></Col>
+            </Row>
+            <Row xs="4" className="mt-3">
+                <Col><Button onClick={() => setDigit('7')} color="primary" className={"btn-block"} >7</Button></Col>
+                <Col><Button onClick={() => setDigit('8')} color="primary" className={"btn-block"} >8</Button></Col>
+                <Col><Button onClick={() => setDigit('9')} color="primary" className={"btn-block"} >9</Button></Col>
+                <Col><Button onClick={() => operation('MUL')} color="warning" className={"btn-block"} >x</Button></Col>
+            </Row>
+            <Row xs="4" className="mt-3">
+                <Col><Button onClick={() => setDigit('.')} color="primary" className={"btn-block"}>.</Button></Col>
+                <Col><Button onClick={() => setDigit('0')} color="primary" className={"btn-block"} >0</Button></Col>
+                <Col><Button onClick={() => operation('ANS')} color="success" className={"btn-block"}>=</Button></Col>
+                <Col><Button onClick={() => operation('DIV')} color="warning" className={"btn-block"}>/</Button></Col>
+            </Row>
+            {/* <ListGroup>
+                <ListGroupItem>
+                    <ListGroup horizontal>
+                        <ListGroupItem><Button onClick={() => setDigit('1')}>1</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => setDigit('2')}>2</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => setDigit('3')}>3</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => operation('ADD')}>+</Button></ListGroupItem>
+                    </ListGroup>
+                </ListGroupItem>
+                <ListGroupItem>
+                    <ListGroup horizontal>
+                        <ListGroupItem><Button onClick={() => setDigit('4')}>4</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => setDigit('5')}>5</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => setDigit('6')}>6</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => operation('SUB')}>-</Button></ListGroupItem>
+                    </ListGroup>
+                </ListGroupItem>
+                <ListGroupItem>
+                    <ListGroup horizontal>
+                        <ListGroupItem><Button onClick={() => setDigit('7')}>7</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => setDigit('8')}>8</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => setDigit('9')}>9</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => operation('MUL')}>x</Button></ListGroupItem>
+                    </ListGroup>
+                </ListGroupItem>
+                <ListGroupItem>
+                    <ListGroup horizontal>
+                        <ListGroupItem><Button onClick={() => setDigit('.')}>.</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => setDigit('0')}>0</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => operation('ANS')}>=</Button></ListGroupItem>
+                        <ListGroupItem><Button onClick={() => operation('DIV')}>/</Button></ListGroupItem>
+                    </ListGroup>
+                </ListGroupItem>
+            </ListGroup> */}
+        </Container>
     )
 }
 
